352ac3a1075deecba5a0eaf9deea5e2277d2d96b
Eldar Eminov <em88el@gmail.com>
2025-09-12 00:30:27 +0300

    Small fix

--- app/jobs/import_products_job.rb
+++ app/jobs/import_products_job.rb
@@ -52,7 +52,7 @@ class ImportProductsJob < ApplicationJob
     row['extra']['color_name'] = row['extra']['color'] == 'Другой' ? color : nil
     row['extra']['length']     = row['extra']['depth'] if row['category'] == 'Кровати'
     row['extra']['furniture_type']           = 'Двуспальная' if row['category'] == 'Кровати'
-    row['extra']['condition_sleeping_place'] = 'Ровное' if row['extra']['sleeping_place'] == 'Eсть'
+    row['extra']['condition_sleeping_place'] = 'Ровное' if row['extra']['sleeping_place'].present?
     row['category']      = 'Комоды и тумбы' if row['category'] == 'Тумбы'
     row[:touched_run_id] = run_id
     row[:deleted]        = 0
