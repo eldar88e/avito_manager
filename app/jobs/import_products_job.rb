82ee709e69746c4394e99eff77e4c2ac01512d99
Eldar Eminov <em88el@gmail.com>
2025-10-20 17:54:37 +0300

    Refactor

--- app/jobs/import_products_job.rb
+++ app/jobs/import_products_job.rb
@@ -51,23 +51,25 @@ class ImportProductsJob < ApplicationJob
   end
 
   def process_product(user, row, run_id, count)
-    row[:md5_hash]             = md5_hash(row.slice(*KEYS).merge(row['extra']))
-    row[:images]               = { first: row.delete('first_image'), other: row.delete('images') }
-    color                      = row['extra']['color']
-    row['extra']['color']      = COLORS.include?(color) ? color : 'Другой'
-    row['extra']['color_name'] = row['extra']['color'] == 'Другой' ? color : nil
-    row['extra']['length']     = row['extra']['depth'] if row['category'] == 'Кровати'
+    row[:md5_hash] = md5_hash(row.slice(*KEYS).merge(row['extra']))
+    row[:images]   = { first: row.delete('first_image'), other: row.delete('images') }
+
+    color                                    = row['extra']['color']
+    row['extra']['color']                    = COLORS.include?(color) ? color : 'Другой'
+    row['extra']['color_name']               = color if row['extra']['color'] == 'Другой'
+    row['extra']['length']                   = row['extra']['depth'] if row['category'] == 'Кровати'
     row['extra']['furniture_type']           = 'Двуспальная' if row['category'] == 'Кровати'
     row['extra']['condition_sleeping_place'] = 'Ровное' if row['extra']['sleeping_place'].present?
     row['extra']['mechanism_condition']      = 'Всё в порядке' if row['extra']['folding_mechanism'].present? && row['extra']['folding_mechanism'] != 'Без механизма'
     row['extra']['sofa_corner']              = 'Универсальный' if row['extra']['furniture_shape'] == 'Угловой'
     row['extra']['cabinet_type']             = 'Прикроватные тумбы' if row['category'] == 'Тумбы'
     row['extra']['material']                 = 'МДФ|Дерево' if row['category'] == 'Тумбы'
-    row['extra']['furniture_frame']          = 'С обивкой' if row['category'] == 'Кровати'
+    row['extra']['furniture_frame']          = 'Дерево|ДСП|С обивкой' if row['category'] == 'Кровати'
     row['extra']['purpose']                  = 'Гостиная' if row['category'] == 'Диваны'
     row['extra']['purpose']                  = 'Гостиная|Детская|Кухня|Офис|Кафе и ресторан' if row['category'] == 'Мини-Диваны'
-    row[:touched_run_id]                     = run_id
-    result                                   = update_product(user, row, count)
+
+    row[:touched_run_id] = run_id
+    result = update_product(user, row, count)
     return if result
 
     row[:run_id] = run_id
