900943378ade80282d1ce8269ffd75d2944e6502
Eldar Eminov <em88el@gmail.com>
2025-11-23 18:19:20 +0300

    Small fix

--- app/jobs/populate_excel_job.rb
+++ app/jobs/populate_excel_job.rb
@@ -124,7 +124,8 @@ class PopulateExcelJob < ApplicationJob
   def formit_title(product, adv)
     prefix = PREFIX[product.category]
     prefix = "#{adv.extra['furniture_type']} #{prefix}" if product.category == 'Кровати' && adv.extra.present?
-    prefix = "#{adv.extra['furniture_shape']} #{prefix}" if product.category == 'Диваны' && adv.extra['furniture_shape'] != 'Прямой'
+    shape  = product.extra['furniture_shape']
+    prefix = "#{shape} #{prefix}" if product.category == 'Диваны' && shape != 'Прямой'
     title  = prefix.present? ? "#{prefix} #{product.title}" : product.title
     title  = build_bed_title(adv, title) if product.category == 'Кровати' && adv.extra.present?
     title
