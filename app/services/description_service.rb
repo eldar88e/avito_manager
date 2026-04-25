d7464f1122e6d3747ed944d23621fd4fab8c0f6b
Eldar Eminov <em88el@gmail.com>
2026-04-25 17:12:03 +0300

    Small fix

--- app/services/description_service.rb
+++ app/services/description_service.rb
@@ -1,8 +1,8 @@
 class DescriptionService
-  MATTRESS_SIZES = [2000, 1800, 1600, 1400, 1200, 1000, 900].freeze
-  MATTRESS_LENGTH = 2000
+  MATTRESS_WIDTH = [200, 180, 160, 140, 120, 100, 90].freeze
+  MATTRESS_LENGTH = 200
   BED_SIZES = [[212, 140], [192, 140], [172, 140], [152, 130], [132, 120], [112, 120], [102, 120]].freeze
-  LENGTH = 2140
+  LENGTH = 214
 
   def initialize(description, replacements)
     @description  = description
@@ -24,15 +24,16 @@ class DescriptionService
   private
 
   def bed_sizes_str(value)
-    str = "🛏 Другие размеры данной кровати:\n\n"
+    str   = "🛏 Другие размеры данной кровати:\n\n"
     count = 1
     BED_SIZES.each_with_index do |size, idx|
       next if size[0] == value
 
-      str += "#{count}. Кровать: #{size[0]}0x#{LENGTH}x#{size[1]}0; Матрас: #{MATTRESS_SIZES[idx]}x#{MATTRESS_LENGTH};\n"
+      mattress_size = "#{MATTRESS_WIDTH[idx]}×#{MATTRESS_LENGTH}"
+      str += "#{count}. Кровать: #{size[0]}×#{LENGTH}×#{size[1]}; Матрас: #{mattress_size};\n"
       count += 1
     end
-    str += "\n**Размеры указанные в миллиметрах"
+    str += "\n**Размеры указанные в сантиметрах"
     str
   end
 end
