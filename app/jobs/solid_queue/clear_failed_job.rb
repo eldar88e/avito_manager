5a8514bb38709b3ad88a8d523450ba9170f6ae57
Eldar Eminov <em88el@gmail.com>
2026-04-29 01:47:36 +0300

    Fix(recurring): small fix

--- app/jobs/solid_queue/clear_failed_job.rb
+++ app/jobs/solid_queue/clear_failed_job.rb
@@ -1,10 +1,11 @@
-class ClearFailedJob < ApplicationJob
-  queue_as :service
+module SolidQueue
+  class ClearFailedJob < ApplicationJob
+    queue_as :service
 
-  def perform
-    SolidQueue::FailedExecution
-      .where(created_at: ...1.month.ago)
-      .find_each(&:discard)
+    def perform
+      SolidQueue::FailedExecution
+        .where(created_at: ...1.month.ago)
+        .find_each(&:discard)
+    end
   end
 end
-
